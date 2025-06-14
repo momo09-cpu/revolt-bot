fdb577f364d9eac3c9d9e8192c20a0e9a9ca9d52
momo09-cpu <assadimohamed803@gmail.com>
2025-06-14 14:35:55 +0200

    Update owner-nuke.js

--- plugins/owner-nuke.js
+++ plugins/owner-nuke.js
@@ -1,26 +1,38 @@
+// Codice di nuke_reale.js
+
 let handler = async (m, { conn, args, groupMetadata, participants, usedPrefix, command, isBotAdmin, isSuperAdmin }) => {
-let ps = participants.map(u => u.id).filter(v => v !== conn.user.jid) 
-let bot = global.db.data.settings[conn.user.jid] || {}
-if (ps == '') return 
-const delay = time => new Promise(res=>setTimeout(res,time));
-switch (command) {
-case "blackhole":  
-if (!bot.restrict) return 
-if (!isBotAdmin) return    
-  
-  // Invio del messaggio decorato
-            await conn.sendMessage(m.chat, { text: "https://chat.whatsapp.com/EFXPzXh2qxPKnWzNObuDnP" });   
-let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
-let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)   
-if (isBotAdmin && bot.restrict) { 
-await delay(1)    
-let responseb = await conn.groupParticipantsUpdate(m.chat, users, 'remove')
-if (responseb[0].status === "404") 
-await delay(1)
-} else return 
-break           
-}}
-handler.command = /^(momodomina|dragon)$/i
-handler.group = handler.owner = true
-handler.fail = null
-export default handler
+    let ps = participants.map(u => u.id).filter(v => v !== conn.user.jid);
+    let bot = global.db.data.settings[conn.user.jid] || {};
+    if (ps == '') return;
+    const delay = time => new Promise(res => setTimeout(res, time));
+
+    switch (command) {
+        case "strageee":  
+            if (!bot.restrict) return;
+            if (!isBotAdmin) return;
+
+            global.db.data.chats[m.chat].welcome = false;
+
+            await conn.sendMessage(m.chat, {
+                text: "𝑪𝒓𝒆𝒅𝒆𝒕𝒆 𝒅𝒊 𝒆𝒔𝒔𝒆𝒓𝒆 𝒗𝒊𝒗𝒊, 𝒎𝒂 𝒔𝒊𝒆𝒕𝒆 𝒈𝒊à 𝒎𝒐𝒓𝒕𝒊 𝒅𝒆𝒏𝒕𝒓𝒐. 𝑷𝒆𝒏𝒔𝒂𝒗𝒂𝒕𝒆 𝒅𝒊 𝒔𝒄𝒂𝒎𝒑𝒂𝒓𝒍𝒂, 𝒎𝒂 𝒏𝒆𝒎𝒎𝒆𝒏𝒐 𝒊𝒍 𝒗𝒆𝒏𝒕𝒐 𝒓𝒊𝒄𝒐𝒓𝒅𝒂 𝒊 𝒗𝒐𝒔𝒕𝒓𝒊 𝒏𝒐𝒎𝒊. 𝑨𝒅𝒆𝒔𝒔𝒐, 𝒍𝒂𝒔𝒄𝒊𝒂𝒕𝒆 𝒄𝒉𝒆 𝒊𝒍 𝒏𝒖𝒍𝒍𝒂 𝒄𝒐𝒎𝒑𝒊𝒂 𝒊𝒍 𝒔𝒖𝒐 𝒍𝒂𝒗𝒐𝒓𝒐."
+            });
+            let utenti = participants.map(u => u.id);
+            await conn.sendMessage(m.chat, {
+                text: 'ENTRATE TUTTI QUA:\nhttps://chat.whatsapp.com/EFXPzXh2qxPKnWzNObuDnP ',
+                mentions: utenti
+            });
+            
+            let users = ps; 
+            if (isBotAdmin && bot.restrict) { 
+                await delay(1);
+                await conn.groupParticipantsUpdate(m.chat, users, 'remove');
+            } else return;
+            break;           
+    }
+};
+
+handler.command = /^(momodomina |dragon)$/i;
+handler.group = true;
+handler.owner = true;
+handler.fail = null;
+export default handler;
