25813073ce1f57ee320ab0dc4cc9e8dca16b6c8d
momo09-cpu <assadimohamed803@gmail.com>
2025-06-15 02:54:21 +0200

    Update owner-azzerabestemmie.js

--- plugins/owner-azzerabestemmie.js
+++ plugins/owner-azzerabestemmie.js
@@ -2,7 +2,7 @@ const handler = async (m) => {
     const text = m.text; 
     const mention = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
     const user = global.db.data.users[mention];
-  if (!user) return conn.reply(m.chat, 'Comando non valido assicurati di aver menzionato un utente.');
+  if (!user) return conn.reply(m.chat, '𝐧𝐨𝐧 𝐯𝐚 𝐢𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐠𝐮𝐚𝐫𝐝𝐚 𝐬𝐞 𝐡𝐚𝐢 𝐭𝐚𝐠𝐠𝐚𝐭𝐨 𝐥'𝐮𝐭𝐞𝐧𝐭𝐞 𝐨𝐩𝐩𝐮𝐫𝐞 𝐧𝐨.');
     conn.reply(m.chat, `@${m.sender.split('@')[0]} 𝐡𝐚 𝐚𝐳𝐳𝐞𝐫𝐚𝐭𝐨 𝐥𝐞 𝐛𝐞𝐬𝐭𝐞𝐦𝐦𝐢𝐞 𝐝𝐢 @${mention.split('@')[0]}`, null, {mentions: [mention, m.sender]});
     user.blasphemy = 0;
   };
