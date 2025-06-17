7dbaacb8f624f3cbfdd4776a041000d965a174e0
momo09-cpu <assadimohamed803@gmail.com>
2025-06-18 00:51:22 +0200

    Update fun-personalità.js

--- plugins/fun-personalità.js
+++ plugins/fun-personalità.js
@@ -7,13 +7,13 @@ let handler = async (m, { conn, command, text }) => {
             forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363259442839354@newsletter',
                 serverMessageId: '',
-                newsletterName: 'ChatUnity'
+                newsletterName: ''
             }
         }
     }, { quoted: m });
 
     let personalita = `
-┏━━°❀❬ *PERSONALITÀ* ❭❀°━━┓
+┏━━°᯽❬ *PERSONALITÀ* ❭᯽°━━┓
 *┃*
 *┃• Nome* : ${text}
 *┃• Moralità Buona* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
