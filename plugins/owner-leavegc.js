fffd13f859c8a801c440ebf026a9f0dbd747fad1
momo09-cpu <assadimohamed803@gmail.com>
2025-06-18 21:20:00 +0200

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, command }) => {
-await m.reply('bye bye') 
+await m.reply('𝒎𝒊 𝒔𝒐𝒏𝒐 𝒓𝒐𝒕𝒕𝒐 𝒊𝒍 𝒄𝒂𝒛𝒛𝒐 𝒅𝒊 𝒔𝒕𝒂𝒓𝒆 𝒒𝒖𝒊 𝒂𝒅𝒅𝒊𝒐 𝒑𝒖𝒕𝒕𝒂𝒏𝒆🐶') 
 await  conn.groupLeave(m.chat)}
 handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
 handler.group = true
