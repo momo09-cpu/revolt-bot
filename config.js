ab8705a9bb7b34f8e6d852e655b3e8939201ecac
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-06-19 21:58:20 +0000

    update

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.confirmCode = ""
 global.owner = [
   ['393534243103', '𝑺𝑼𝑷𝑹𝑬𝑴𝑶', true],
   ['639514776359', '𝑀𝑌 𝐺', true],
-  ['xxxxxxxxxx',], // al posto del X metti il tuo numero di telefono
+  ['393515533859',], // al posto del X metti il tuo numero di telefono
   ['xxxxxxxxxx'],	
   ['xxxxxxxxxx',],
   ['xxxxxxxxxx'],
