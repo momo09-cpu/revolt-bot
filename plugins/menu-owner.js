ea043be45bbdce4ffb2e68f3f7046bb0eaa4d308
momo09-cpu <assadimohamed803@gmail.com>
2025-06-20 13:09:32 +0200

    Update menu-owner.js

--- plugins/menu-owner.js
+++ plugins/menu-owner.js
@@ -26,7 +26,7 @@ const handler = async (message, { conn, usedPrefix, command }) => {
 
     const menuText = generateMenuText(usedPrefix, botName, userCount);
 
-    const imagePath = path.join(__dirname, '../menu/revolt3.mp4'); 
+    const imagePath = path.join(__dirname, '../menu/revolt3.jpg'); 
     await conn.sendMessage(
         message.chat,
         {
