9bf4dbee044d813b05050509710a84309a15954a
momo09-cpu <assadimohamed803@gmail.com>
2025-06-21 20:38:02 +0200

    Update gb-link.js

--- plugins/gb-link.js
+++ plugins/gb-link.js
@@ -1,18 +1,26 @@
 let handler = async (m, { conn, args }) => {
-let group = m.chat
-let prova = { "key": {"participants":"0@s.whatsapp.net", "fromMe": false, "id": "Halo"
-  }, "message": {
-  "locationMessage": { name: '𝐋͎𝕀𝐍𝐊 𝐃𝚵𝐋͎ 𝐆𝐑𝐔𝐏𝐏Ꮻ 🔗',
-"jpegThumbnail": fs.readFileSync('./icone/link.png'),
-"vcard":
-`BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-}}, "participant": "0@s.whatsapp.net"
-}
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, link, prova, m, {detectLink: true})
-//conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })
-}
-handler.command = /^link(gro?up)?$/i
-handler.group = true
-handler.botAdmin = true
-export default handler
+    let group = m.chat
+    let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+
+    // Get bot name from database or use default
+    let nomeDelBot = global.db.data.nomedelbot || `𝙧ᥱ𝙫𝗈ᶩէ💠`
+
+    await conn.sendMessage(m.chat, { 
+      text: link,
+      contextInfo: {
+        forwardingScore: 99,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363259442839354@newsletter',
+          serverMessageId: '',
+          newsletterName: `${𝙧ᥱ𝙫𝗈ᶩէ💠}`
+        }
+      }
+    }, { quoted: m, detectLink: true })
+  }
+
+  handler.command = /^link(gro?up)?$/i
+  handler.admin = true
+  handler.group = true
+  handler.botAdmin = true
+  export default handler
